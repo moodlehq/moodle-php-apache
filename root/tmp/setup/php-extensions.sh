ad324e15df996b65fd9f7cc67735428e38d8feff
Dan Poltawski <dan@moodle.com>
2017-05-23 15:28:03 +0100

    Reduce image size by clean up depends after building

--- root/tmp/setup/php-extensions.sh
+++ root/tmp/setup/php-extensions.sh
@@ -4,28 +4,14 @@ set -e
 
 echo "Installing apt depdencies"
 
+BUILD_PACKAGES="gettext libcurl4-openssl-dev libpq-dev libmysqlclient-dev libldap2-dev libxslt-dev \
+    libxml2-dev libicu-dev libfreetype6-dev libjpeg62-turbo-dev libmemcached-dev \
+    zlib1g-dev libpng12-dev locales unixodbc-dev"
+
+LIBS="libaio1 libcurl3 libgss3 libicu52 libmysqlclient18 libpq5 libmemcached11 libmemcachedutil2 libldap-2.4-2 libxml2 libxslt1.1 unixodbc"
 
 apt-get update
-apt-get install -y \
-    gettext \
-    libcurl4-openssl-dev \
-    libpq-dev \
-    libmysqlclient-dev \
-    libldap2-dev \
-    libxslt-dev \
-    libxml2-dev \
-    libicu-dev \
-    libfreetype6-dev \
-    libjpeg62-turbo-dev \
-    libmcrypt-dev \
-    libmemcached-dev \
-    zlib1g-dev \
-    libpng12-dev \
-    libaio1 \
-    unzip \
-    ghostscript \
-    locales
-
+apt-get install -y --no-install-recommends $BUILD_PACKAGES $LIBS unzip ghostscript locales
 echo 'Generating locales..'
 echo 'en_US.UTF-8 UTF-8' > /etc/locale.gen
 echo 'en_AU.UTF-8 UTF-8' >> /etc/locale.gen
@@ -52,3 +38,21 @@ pecl install solr memcache redis mongodb igbinary apcu-4.0.11 memcached-2.2.0
 docker-php-ext-enable solr memcache memcached redis mongodb apcu igbinary
 
 echo 'apc.enable_cli = On' >> /usr/local/etc/php/conf.d/docker-php-ext-apcu.ini
+
+echo "Downloading freedts files"
+curl ftp://ftp.freetds.org/pub/freetds/stable/freetds-1.00.33.tar.gz -o /tmp/freetds-1.00.33.tar.gz
+
+echo "Building mssql extension"
+cd /tmp && tar -xvf freetds-1.00.33.tar.gz && cd freetds-1.00.33 \
+    && ./configure --with-unixodbc=/usr --sysconfdir=/etc/freetds --enable-sybase-compat \
+    && make -j$(nproc) \
+    && make install
+
+docker-php-ext-install -j$(nproc) mssql
+
+# Keep our image size down..
+pecl clear-cache
+apt-get remove --purge -y $BUILD_PACKAGES
+apt-get autoremove -y
+apt-get clean
+rm -rf /var/lib/apt/lists/*
