5658c16f22858db0600288e43d3809964724832f
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2019-07-24 18:11:42 +0200

    First cut of php 7.4 (alpha3) docker image

--- root/tmp/setup/php-extensions.sh
+++ root/tmp/setup/php-extensions.sh
@@ -51,7 +51,7 @@ docker-php-ext-install -j$(nproc) \
     xmlrpc
 
 # GD.
-docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
+docker-php-ext-configure gd --with-freetype=/usr/include/ --with-jpeg=/usr/include/
 docker-php-ext-install -j$(nproc) gd
 
 # LDAP.
@@ -63,7 +63,7 @@ pecl install memcached mongodb redis apcu igbinary
 docker-php-ext-enable memcached mongodb redis apcu igbinary
 
 # ZIP
-docker-php-ext-configure zip --with-libzip
+docker-php-ext-configure zip --with-zip
 docker-php-ext-install zip
 
 echo 'apc.enable_cli = On' >> /usr/local/etc/php/conf.d/docker-php-ext-apcu.ini
