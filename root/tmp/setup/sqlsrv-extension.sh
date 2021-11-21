5e7e743f663b020b2b1b995d3a05730d21c7a988
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2021-11-21 16:01:47 +0100

    Switch to debian 9 (stretch) packages and bump to PHP 7.3.33

--- root/tmp/setup/sqlsrv-extension.sh
+++ root/tmp/setup/sqlsrv-extension.sh
@@ -3,8 +3,6 @@
 set -e
 
 # Install Microsoft dependencies for sqlsrv
-# Debian 9 requires ODBC driver 17, still not package available in repos, so followed this
-# https://github.com/Microsoft/msphpsql/wiki/Install-and-configuration#user-content-odbc-17-linux-installation
 echo "Downloading sqlsrv files"
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
 curl https://packages.microsoft.com/config/debian/9/prod.list -o /etc/apt/sources.list.d/mssql-release.list
