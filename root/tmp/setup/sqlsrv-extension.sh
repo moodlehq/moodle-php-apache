e82801494a22ebf3f8ce3ea18fd9e359de93791d
Andrew Nicols <andrew@nicols.co.uk>
2023-03-15 10:32:51 +0800

    Add support for ARM builds of sqlsrv

--- root/tmp/setup/sqlsrv-extension.sh
+++ root/tmp/setup/sqlsrv-extension.sh
@@ -2,11 +2,6 @@
 
 set -e
 
-if [[ ${TARGETPLATFORM} != "linux/amd64" ]]; then
-  echo "sqlsrv extension not available for ${TARGETPLATFORM} architecture, skipping"
-  exit 0
-fi
-
 # Packages for build.
 BUILD_PACKAGES="gnupg unixodbc-dev"
 
@@ -24,17 +19,16 @@ apt-get install -y --no-install-recommends apt-transport-https \
 # Install Microsoft dependencies for sqlsrv
 echo "Downloading sqlsrv files"
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-# TODO, bullseye should be 11, but the msodbcsql17 package is not available yet, hence using buster (10) one.
-curl https://packages.microsoft.com/config/debian/10/prod.list -o /etc/apt/sources.list.d/mssql-release.list
+curl https://packages.microsoft.com/config/debian/11/prod.list -o /etc/apt/sources.list.d/mssql-release.list
 apt-get update
 
 echo "Install msodbcsql"
-ACCEPT_EULA=Y apt-get install -y msodbcsql17
+ACCEPT_EULA=Y apt-get install -y msodbcsql18
 
 ln -fsv /opt/mssql-tools/bin/* /usr/bin
 
-# Need 5.10.1 (or later) for PHP 8.2 support
-pecl install sqlsrv-5.10.1
+# Need 5.11.0 (or later) for PHP 8.2 support
+pecl install sqlsrv-5.11.0
 docker-php-ext-enable sqlsrv
 
 # Keep our image size down..
