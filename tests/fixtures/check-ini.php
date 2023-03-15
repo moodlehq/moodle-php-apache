0cf00d065386d4fd50f72e2e3e5e450a76fe64bc
Andrew Nicols <andrew@nicols.co.uk>
2023-03-15 21:55:40 +0800

    Add simple environment-based PHP configuration
    This adds a layer of common sense on top of the other changes I've
    worked on to allow easy configuration variable setting.
    
    Fixes #166

--- tests/fixtures/check-ini.php
+++ tests/fixtures/check-ini.php
@@ -1,20 +1,42 @@
 <?php
 
-$uploadsEnabled = ini_get('file_uploads');
+$fileuploads = ini_get('file_uploads');
+$apcenabled = ini_get('apc.enabled');
+$memorylimit = ini_get('memory_limit');
+
+$allokay = true;
+$message = [];
+if (!empty($fileuploads)) {
+    $allokay = false;
+    $message[] = "Uploads are enabled and should be disabled.";
+    $message[] = var_export($fileuploads, true);
+}
+
+if (!empty($apcenabled)) {
+    $allokay = false;
+    $message[] = "apc.enabled is not Off (0): ({$apcenabled})";
+}
+
+if ($memorylimit !== '256M') {
+    $allokay = false;
+    $message[] = "Memory limit not set to 256M: ({$memorylimit})";
+}
 
 if (php_sapi_name() === 'cli') {
-    if (empty($uploadsEnabled)) {
+    if ($allokay) {
         echo "OK\n";
         exit(0);
     }
-    echo "Uploads are enabled and should be disabled.";
-    var_dump($uploadsEnabled);
+
+    echo implode("\n", $message) . "\n";
     exit(1);
 } else {
-    if (empty($uploadsEnabled)) {
+    if ($allokay) {
         header('HTTP/1.1 200 - OK');
         exit(0);
     }
-    header('HTTP/1.1 500 - Uploads are enabled and should be disabled: ' . var_export($uploadsEnabled, true));
+
+    header('HTTP/1.1 500 - ' . implode(", ", $message));
+    echo implode("<br>", $message);
     exit(1);
 }
