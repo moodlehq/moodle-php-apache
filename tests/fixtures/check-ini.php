74c794a355b7edbbc85dbf182c08a0eab711ba08
Andrew Nicols <andrew@nicols.co.uk>
2023-03-19 22:25:10 +0800

    Increase max upload size
    This increases the standard maximum upload file size to 200M, which is a
    much more realistic size for development.
    
    This also requires a bump in the maximum post size.

--- tests/fixtures/check-ini.php
+++ tests/fixtures/check-ini.php
@@ -1,17 +1,17 @@
 <?php
 
-$fileuploads = ini_get('file_uploads');
-$apcenabled = ini_get('apc.enabled');
-$memorylimit = ini_get('memory_limit');
 
 $allokay = true;
 $message = [];
+
+$fileuploads = ini_get('file_uploads');
 if (!empty($fileuploads)) {
     $allokay = false;
     $message[] = "Uploads are enabled and should be disabled.";
     $message[] = var_export($fileuploads, true);
 }
 
+$apcenabled = ini_get('apc.enabled');
 if (!empty($apcenabled)) {
     $allokay = false;
     $message[] = "apc.enabled is not Off (0): ({$apcenabled})";
@@ -23,6 +23,7 @@ if (!empty($apcenabledcli)) {
     $message[] = "apc.enabled_cli is not Off (0): ({$apcenabledcli})";
 }
 
+$memorylimit = ini_get('memory_limit');
 if ($memorylimit !== '256M') {
     $allokay = false;
     $message[] = "Memory limit not set to 256M: ({$memorylimit})";
@@ -32,6 +33,18 @@ $pcovenabled = ini_get('pcov.enabled');
 if (empty($pcovenabled)) {
     $allokay = false;
     $message[] = "pcov.enabled is not On (1): ({$pcovenabled})";
+}
+
+$maxuploadsize = ini_get('upload_max_filesize');
+if ($maxuploadsize !== '20M') {
+    $allokay = false;
+    $message[] = "Maximum upload size not set to 20M: ({$maxuploadsize})";
+}
+
+$postmaxsize = ini_get('post_max_size');
+if ($postmaxsize !== '206M') {
+    $allokay = false;
+    $message[] = "Maximum post size not set to 206M: ({$postmaxsize})";
 }
 
 if (php_sapi_name() === 'cli') {
