e61c4ec148d94d9ccc201b163dcb1a79772a9705
Andrew Nicols <andrew@nicols.co.uk>
2023-03-19 23:01:53 +0800

    Rename other moodle ini files for better lexical sort

--- root/tmp/setup/php-extensions.sh
+++ root/tmp/setup/php-extensions.sh
@@ -67,12 +67,12 @@ docker-php-ext-install -j$(nproc) ldap
 pecl install apcu igbinary memcached pcov redis solr timezonedb uuid xmlrpc-beta
 docker-php-ext-enable apcu igbinary memcached pcov redis solr timezonedb uuid xmlrpc
 
-echo 'apc.enable_cli = On' >> /usr/local/etc/php/conf.d/docker-php-ext-apcu.ini
+echo 'apc.enable_cli = On' >> /usr/local/etc/php/conf.d/10-docker-php-ext-apcu.ini
 
-echo "pcov.enabled=0" >> /usr/local/etc/php/conf.d/docker-php-ext-pcov.ini
-echo "pcov.exclude='~\/(tests|coverage|vendor|node_modules)\/~'" >> /usr/local/etc/php/conf.d/docker-php-ext-pcov.ini
-echo "pcov.directory=." >> /usr/local/etc/php/conf.d/docker-php-ext-pcov.ini
-echo "pcov.initial.files=1024" >> /usr/local/etc/php/conf.d/docker-php-ext-pcov.ini
+echo "pcov.enabled=0" >> /usr/local/etc/php/conf.d/10-docker-php-ext-pcov.ini
+echo "pcov.exclude='~\/(tests|coverage|vendor|node_modules)\/~'" >> /usr/local/etc/php/conf.d/10-docker-php-ext-pcov.ini
+echo "pcov.directory=." >> /usr/local/etc/php/conf.d/10-docker-php-ext-pcov.ini
+echo "pcov.initial.files=1024" >> /usr/local/etc/php/conf.d/10-docker-php-ext-pcov.ini
 
 # Keep our image size down..
 pecl clear-cache
