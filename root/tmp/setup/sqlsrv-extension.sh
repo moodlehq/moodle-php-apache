01e3b2b3bab58368311b1326cbde5d373d3c7406
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2023-08-13 16:24:55 +0200

    Use bullseye repo for sqlsrv until bookworm one is available
    see https://learn.microsoft.com/en-us/answers/questions/1328834/debian-12-public-key-is-not-available

--- root/tmp/setup/sqlsrv-extension.sh
+++ root/tmp/setup/sqlsrv-extension.sh
@@ -24,7 +24,8 @@ apt-get install -y --no-install-recommends apt-transport-https \
 # Install Microsoft dependencies for sqlsrv
 echo "Downloading sqlsrv files"
 curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add -
-# TODO, bookworm should be 12, but the msodbcsql17 package is not available yet, hence using buster (10) one.
+# TODO, bookworm should be 12, but the msodbcsql17 package is not available yet, hence using bullseye (11) one.
+# (see https://learn.microsoft.com/en-us/answers/questions/1328834/debian-12-public-key-is-not-available)
 curl https://packages.microsoft.com/config/debian/10/prod.list -o /etc/apt/sources.list.d/mssql-release.list
 apt-get update
 
