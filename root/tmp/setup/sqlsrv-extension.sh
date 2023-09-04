17481c57c56bf679a013ecccd789bc285b9a9c8c
Eloy Lafuente (stronk7) <stronk7@moodle.org>
2023-09-04 12:35:50 +0200

    Bump to php-sqlsrv 5.11
    - It's required for PHP >= 8.2
    - It also works for PHP 8.0 and 8.1

--- root/tmp/setup/sqlsrv-extension.sh
+++ root/tmp/setup/sqlsrv-extension.sh
@@ -32,8 +32,8 @@ ACCEPT_EULA=Y apt-get install -y msodbcsql17
 
 ln -fsv /opt/mssql-tools/bin/* /usr/bin
 
-# Need 5..10.0beta1 (or later) for PHP 8.1 support
-pecl install sqlsrv-5.10.0
+# Need 5.10 (or later) for PHP 8.1 support
+pecl install sqlsrv-5.11.0
 docker-php-ext-enable sqlsrv
 
 # Keep our image size down..
