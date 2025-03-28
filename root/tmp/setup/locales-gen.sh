1ea0e5b83d958f94adca1f159e8b7c94194b55a6
Andrew Nicols <andrew@nicols.co.uk>
2025-03-28 08:49:20 +0800

    Use locales-all instead of building all locales every build

--- root/tmp/setup/locales-gen.sh
+++ root/tmp/setup/locales-gen.sh
@@ -5,17 +5,11 @@ set -e
 echo "Installing apt dependencies"
 
 # Packages for installing locales.
-RUNTIME_LOCALES="locales"
+RUNTIME_LOCALES="locales locales-all"
 
 apt-get update
 apt-get install -y --no-install-recommends apt-transport-https \
     $RUNTIME_LOCALES
-
-echo "Installing UTF-8 locales"
-
-# Generate the locales configuration for all possible UTF-8 locales.
-grep UTF-8 /usr/share/i18n/SUPPORTED > /etc/locale.gen
-locale-gen
 
 # Keep our image size down..
 apt-get autoremove -y
